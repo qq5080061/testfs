ad0569907bf999944cae9161ad7bcfbabdb7c6e3
paas <paas@cse-codegen-backend-252369159-277jh>
2017-11-16 01:48:25 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/controller/TestfsImpl.java
@@ -0,0 +1,65 @@
+package com.service.controller;
+
+import com.service.model.Person;
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import io.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringCodegen", date = "2017-11-16T01:48:25.507Z")
+
+@RestSchema(schemaId = "testfs")
+@RequestMapping(path = "/testfs", produces = MediaType.APPLICATION_JSON)
+public class TestfsImpl {
+
+    @Autowired
+    private TestfsDelegate userTestfsDelegate;
+
+
+    @RequestMapping(value = "/add",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public Integer add( @RequestParam(value = "a", required = true) Integer a, @RequestParam(value = "b", required = true) Integer b){
+
+        return userTestfsDelegate.add(a, b);
+    }
+
+
+    @RequestMapping(value = "/sayhei",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHei( @RequestHeader(value="name", required=true) String name){
+
+        return userTestfsDelegate.sayHei(name);
+    }
+
+
+    @RequestMapping(value = "/sayhello/{name}",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String sayHello( @PathVariable("name") String name){
+
+        return userTestfsDelegate.sayHello(name);
+    }
+
+
+    @RequestMapping(value = "/sayhi",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHi( @RequestParam(value = "name", required = true) String name){
+
+        return userTestfsDelegate.sayHi(name);
+    }
+
+
+    @RequestMapping(value = "/saysomething",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String saySomething( @RequestParam(value = "prefix", required = true) String prefix, @RequestBody Person user){
+
+        return userTestfsDelegate.saySomething(prefix, user);
+    }
+
+}
